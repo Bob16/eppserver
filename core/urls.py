fb8cef25fd37ab1f70c551645b54c0830d58f99e
Bablu Vazhayil Vijayakumar <babluvijayakumar@Bablus-MacBook-Pro.local>
2025-09-07 23:50:51 +0100

    Add root dashboard route and ensure login required

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.dashboard, name='home'),  # This makes / show the dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
